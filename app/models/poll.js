fb52ac8bd5b67f895cdb065d9bc392dedf87e3c4
majorazero <daniel.cw.hsu@gmail.com>
2017-04-18 20:47:21 -0700

    Added result sorting.
    -Results will now display options with the most votes to least votes
    descending.

--- app/models/poll.js
+++ app/models/poll.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
-  voteCount: Ember.computed.alias('votes.length') //calculates the total amount of votes the array has
+  voteCount: Ember.computed.alias('votes.length'), //calculates the total amount of votes the array has
+  optionsSorting: ['voteCount:desc'], //dictates how it'll be sorted
+  sortedOptions: Ember.computed.sort('options','optionsSorting') //the actual sorting code
 });
