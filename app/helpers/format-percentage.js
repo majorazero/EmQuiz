06720053dd90ff369391a9f4a46303fcc6c24d60
majorazero <daniel.cw.hsu@gmail.com>
2017-04-18 20:28:13 -0700

    Removed my own crappy percentage helper
    -Installed an ember-cli-format-number instead. Seems better.

--- app/helpers/format-percentage.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function formatPercentage(number) {
-  return `${number*100}%`; //this is a es2015 BACKTICK `` neat little trick
-}
-
-export default Ember.Helper.helper(formatPercentage);
